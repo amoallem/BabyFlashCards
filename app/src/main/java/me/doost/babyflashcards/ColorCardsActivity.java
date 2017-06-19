15266a984bc3e16774871b9b199415cfc44806a6
azin <azin.moallem@gmail.com>
2017-06-19 10:56:35 -0400

    added orange color

--- app/src/main/java/me/doost/babyflashcards/ColorCardsActivity.java
+++ app/src/main/java/me/doost/babyflashcards/ColorCardsActivity.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
+
 import com.doost.babyflashcards.R;
 
 public class ColorCardsActivity extends CardsActivity {
@@ -16,8 +18,10 @@ public class ColorCardsActivity extends CardsActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent callerIntent = getIntent();
-        int colorImages[] = {Color.RED, Color.BLUE, Color.GREEN, Color.BLACK, Color.YELLOW, Color.WHITE};
-        int colorTexts[] = {R.string.red, R.string.blue, R.string.green, R.string.black, R.string.yellow, R.string.white};
+
+        Color.rgb(14, 133, 35);
+        int colorImages[] = {Color.RED, Color.BLUE, Color.GREEN, Color.BLACK, Color.YELLOW, Color.WHITE,  Color.rgb(255,165,0)};
+        int colorTexts[] = {R.string.red, R.string.blue, R.string.green, R.string.black, R.string.yellow, R.string.white, R.string.orange};
 
         ViewGroup viewFlipperAnimalCards = (ViewGroup) findViewById(R.id.viewFlipper_color_cards);
 
